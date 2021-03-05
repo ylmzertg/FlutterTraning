94117f6f8949e89d38d31c0760559942f0ed19d0
ertugrul.yilmaz <ylmzertg@hotmail.com>
2021-03-05 14:41:11 +0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,77 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Uygulama Çerçevesi',
+      home: Iskele(),
+    );
+  }
+}
+
+class Iskele extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Boş Uygulama Çerçevesi'),
+      ),
+      body: AnaEkran(),
+    );
+  }
+}
+
+//satetuful yapısı ekran her yuklemede yenıden derlenır.Ama Statelessde uygulama sonlanana kadar bırkez derlenıp okunur.
+
+class AnaEkran extends StatefulWidget {
+  @override
+  _AnaEkranState createState() => _AnaEkranState();
+}
+
+class _AnaEkranState extends State<AnaEkran> {
+  String blogYazisi = 'Hoş Geldin Bro';
+
+  martGoster() {
+    // blogYazisi = 'Temsili yeni bir yazı';
+    setState(() {
+      blogYazisi = 'Temsili yeni bir test';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(blogYazisi),
+            RaisedButton(
+              onPressed: martGoster,
+              child: Text('Mart Yazısı'),
+            ),
+            RaisedButton(
+              onPressed: martGoster,
+              child: Text('Nisan Yazısı'),
+            ),
+            RaisedButton(
+              onPressed: martGoster,
+              child: Text('Mayıs Yazısı'),
+            ),
+            RaisedButton(
+              onPressed: martGoster,
+              child: Text('Haziran Yazısı'),
+            ),
+            RaisedButton(
+              onPressed: martGoster,
+              child: Text('Temmuz Yazısı'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
